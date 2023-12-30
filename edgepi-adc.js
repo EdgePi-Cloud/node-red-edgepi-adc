6302d15f2ed941a961e53ddc937a809e6c8a8a36
Josiah Kievit <josiahtesfu@gmail.com>
2023-12-29 17:48:44 -0800

    Refactor and update input implementation.

--- edgepi-adc.js
+++ edgepi-adc.js
@@ -4,40 +4,45 @@ module.exports = async function (RED) {
   function AdcNode(config) {
     RED.nodes.createNode(this, config);
     const node = this;
-    let read = config.read;
-    let channel = config.channel;
-    let diff = config.diff;
+    let read = config.read.toLowerCase();
+    let inputSelection = read === "voltage" ? config.channel : config.diff;
     let adcNum = config.adcNum;
-    let adc1DataRate = config.adc1DataRate;
-    let adc2DataRate = config.adc2DataRate;
+    let dataRate = adcNum === 1 ? config.adc1DataRate : config.adc2DataRate;
 
     initializeNode(config)
-      .then(() => {
+      .then((adc) => {
         node.on("input", async function (msg, send, done) {
           node.status({ fill: "green", shape: "dot", text: "input recieved" });
           try {
-            let response;
             read = msg.read ?? read;
-            if (read === "voltage" || read === "differential") {
-              response = await adc.readVoltage(adc-1);
-            } else if (read === "RTD") {
+            adcNum = msg.adc ?? adcNum;
+            inputSelection = msg.payload || inputSelection;
+            dataRate = msg.dataRate ?? dataRate;
+
+            if (read !== "rtd") {
+              await adc.setRtd(false, adcNum - 1);
+            }
+            await setConfigurations(adc);
+
+            let response;
+            if (read === "rtd") {
               response = await adc.readRtdTemperature();
+            } else if (read === "voltage" || read === "differential") {
+              response = await adc.readVoltage(adcNum - 1);
             }
 
-            msg.payload = response;
+            msg = { payload: response };
           } catch (error) {
-            msg.payload = error;
+            msg = { payload: error };
             console.error(error);
           }
-
           send(msg);
-
           done?.();
         });
 
         node.on("close", async () => {
-          await adc.stopConversions(false, rpc.ADCNum[config.adc]);
-          await adc.setRtd(false, rpc.ADCNum.ADC_2);
+          await adc.stopConversions(false, rpc.ADCNum[adcNum]);
+          await adc.setRtd(false, adcNum - 1);
         });
       })
       .catch((error) => {
@@ -49,13 +54,13 @@ module.exports = async function (RED) {
         config.transport === "Network"
           ? `tcp://${config.tcpAddress}:${config.tcpPort}`
           : "ipc:///tmp/edgepi.pipe";
-
       try {
         const adc = new rpc.AdcService(transport);
         console.info("ADC node initialized on:", transport);
         node.status({ fill: "green", shape: "ring", text: "adc initialized" });
-        await setInitialConfigurations(adc, config);
-        await adcService.startConversions(adcNum - 1);
+        await setConfigurations(adc, config);
+        await adc.startConversions(adcNum - 1);
+        return adc;
       } catch (error) {
         console.error(error);
         node.status({
@@ -66,25 +71,19 @@ module.exports = async function (RED) {
       }
     }
 
-    async function setInitialConfigurations(adc, config) {
-      if (config.read === "voltage") {
-        const adcConfigArg = {};
-        if (adcNum === 1) {
-          adcConfigArg.adc_1AnalogIn = channel - 1;
-          adcConfigArg.adc_1DataRate = config.adc1DataRate;
-        } else {
-          adcConfigArg.adc_2AnalogIn = channel - 1;
-          adcConfigArg.adc_2DataRate = config.adc2DataRate;
-        }
-        adcConfigArg.conversionMode = rpc.ConvMode.CONTINUOUS;
-        await adc.setConfig(adcConfigArg);
+    async function setConfigurations(adc) {
+      if (read === "voltage") {
+        const dataRateStr = "SPS_" + dataRate.toString().replace(".", "P");
+        const configArg = {
+          [`adc_${adcNum}AnalogIn`]: inputSelection - 1,
+          [`adc_${adcNum}DataRate`]: dataRateStr,
+          conversionMode: rpc.ConvMode.CONTINUOUS,
+        };
+        await adc.setConfig(configArg);
       } else if (read === "differential") {
-        const diff = rpc.DiffMode[config.diff];
-        await adc.selectDifferential(adcNum - 1, diff);
-      } else if (read === "RTD") {
+        await adc.selectDifferential(adcNum - 1, inputSelection - 1);
+      } else if (read === "rtd") {
         await adc.setRtd(true, adcNum - 1);
-      } else {
-        throw new Error(`Unknown read method: ${config.method}`);
       }
     }
   }
